0ea5787036212d11a06322fa3e816a8c334fb4b8
Wojciech Langiewicz <wlangiewicz@gmail.com>
2014-10-03 16:52:31 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,16 @@
+organization := "com.wlangiewicz"
+
+name := "dump-wallet"
+
+version := "1.0"
+
+scalaVersion := "2.11.2"
+
+resolvers += (
+  "Local Maven Repository" at "file:///"+Path.userHome.absolutePath+"/.m2/repository"
+)
+
+libraryDependencies += "org.scala-sbt" % "command" % "0.13.6"
+
+libraryDependencies += "org.bitcoinj" % "bitcoinj-core" % "0.12-SNAPSHOT"
+
---
+++ src/main/scala/DumpWallet.scala
@@ -0,0 +1,22 @@
+package com.wlangiewicz
+
+import java.io.File
+
+import org.bitcoinj.core.Wallet
+
+object DumpWallet extends App {
+
+  def performDump(path: String): Unit = {
+    val wallet: Wallet  = Wallet.loadFromFile(new File(path))
+    Console.println(wallet.toString(true, true, true, null))
+  }
+
+  override def main(args: Array[String]): Unit = {
+    if(args.length != 1){
+      Console.println("Usage: scala DumpWallet <path>")
+    }
+    else{
+      performDump(args(0))
+    }
+  }
+}
